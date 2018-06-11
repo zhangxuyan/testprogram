ab4bb05d69b952fa9009dee9d501cf667bde0d33
zhangxy <zhangxy@esage.cn>
2018-06-11 13:55:56 +0800

    gengxin

---
+++ testpython/src/com/esage/bean/EOUtility.java
@@ -0,0 +1,67 @@
+package com.esage.bean;
+
+import java.util.List;
+
+public class EOUtility {
+
+	public EOUtility(BaseEO baseEO) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String beanToString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean equalsBean(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int hashCodeBean() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object cloneBean() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String[] getAttributeNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getAttributeValue(String attributeName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setAttributeValue(String attributeName, Object value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getEnumDescription(Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTableName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getDifferentField(BaseEO anotherBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void loadLazyField() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
