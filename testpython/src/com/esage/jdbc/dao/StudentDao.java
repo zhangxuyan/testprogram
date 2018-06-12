ff921cc7529e1bb8530fe772a3395d0d2721b9ba
zhangxy <zhangxy@esage.cn>
2018-06-12 13:55:49 +0800

    jdbc

--- testpython/src/com/esage/jdbc/dao/StudentDao.java
+++ testpython/src/com/esage/jdbc/dao/StudentDao.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 import com.esage.jdbc.bean.*;
 
 public class StudentDao {
-	
-	public  static int insert(Student student) {
+
+	public static int insert(Student student) {
 		Connection conn = DbUtil.getConn();
 		int i = 0;
 		String sql = "insert into students (Name,Sex,Age) values(?,?,?)";
@@ -27,66 +27,63 @@ public class StudentDao {
 		}
 		return i;
 	}
-	
-	
+
 	public static int update(Student student) {
-	    Connection conn = DbUtil.getConn();
-	    int i = 0;
-	    String sql = "update students set Age='" + student.getAge() + "' where Name='" + student.getName() + "'";
-	    PreparedStatement pstmt;
-	    try {
-	        pstmt = (PreparedStatement) conn.prepareStatement(sql);
-	        i = pstmt.executeUpdate();
-	        System.out.println("resutl: " + i);
-	        pstmt.close();
-	        conn.close();
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
-	    return i;
+		Connection conn = DbUtil.getConn();
+		int i = 0;
+		String sql = "update students set Age='" + student.getAge() + "' where Name='" + student.getName() + "'";
+		PreparedStatement pstmt;
+		try {
+			pstmt = (PreparedStatement) conn.prepareStatement(sql);
+			i = pstmt.executeUpdate();
+			System.out.println("resutl: " + i);
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return i;
 	}
-	
-	
+
 	public static Integer getAll() {
-	    Connection conn = DbUtil.getConn();
-	    String sql = "select * from students";
-	    PreparedStatement pstmt;
-	    try {
-	        pstmt = (PreparedStatement)conn.prepareStatement(sql);
-	        ResultSet rs = pstmt.executeQuery();
-	        int col = rs.getMetaData().getColumnCount();
-	        System.out.println("============================");
-	        while (rs.next()) {
-	            for (int i = 1; i <= col; i++) {
-	                System.out.print(rs.getString(i) + "\t");
-	                if ((i == 2) && (rs.getString(i).length() < 8)) {
-	                    System.out.print("\t");
-	                }
-	             }
-	            System.out.println("");
-	        }
-	            System.out.println("============================");
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
-	    return null;
+		Connection conn = DbUtil.getConn();
+		String sql = "select * from students";
+		PreparedStatement pstmt;
+		try {
+			pstmt = (PreparedStatement) conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			int col = rs.getMetaData().getColumnCount();
+			System.out.println("============================");
+			while (rs.next()) {
+				for (int i = 1; i <= col; i++) {
+					System.out.print(rs.getString(i) + "\t");
+					if ((i == 2) && (rs.getString(i).length() < 8)) {
+						System.out.print("\t");
+					}
+				}
+				System.out.println("");
+			}
+			System.out.println("============================");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
-	
-	
+
 	public static int delete(String name) {
-	    Connection conn =DbUtil. getConn();
-	    int i = 0;
-	    String sql = "delete from students where Name='" + name + "'";
-	    PreparedStatement pstmt;
-	    try {
-	        pstmt = (PreparedStatement) conn.prepareStatement(sql);
-	        i = pstmt.executeUpdate();
-	        System.out.println("resutl: " + i);
-	        pstmt.close();
-	        conn.close();
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
-	    return i;
+		Connection conn = DbUtil.getConn();
+		int i = 0;
+		String sql = "delete from students where Name='" + name + "'";
+		PreparedStatement pstmt;
+		try {
+			pstmt = (PreparedStatement) conn.prepareStatement(sql);
+			i = pstmt.executeUpdate();
+			System.out.println("resutl: " + i);
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return i;
 	}
 }
